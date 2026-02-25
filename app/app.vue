437157fdfa2719037af433cea4875b79f140afd3
sergey-kolesnik <00volchenok0013@mail.ru>
2026-02-25 17:04:05 +0300

    create project, add scss/_font,_var,main,normilize.scss

---
+++ app/app.vue
@@ -0,0 +1,16 @@
+<script>
+  let a = 1
+  a += 1
+  function as() {
+    let d = a
+    console.log(d)
+  }
+  as()
+</script>
+
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
