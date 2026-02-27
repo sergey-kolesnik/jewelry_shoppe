e9dd402b635285f343d72d5c981b2dc96ea62b7c
sergey-kolesnik <00volchenok0013@mail.ru>
2026-02-27 12:27:32 +0300

    refactor(.gitignore, useApiFetch): обновил файл gitignore, импортированные модули, добавил вычисляемое свойство
    Обновлен список игнорируемых файлов в проекте (.gitignore),
    добавил необходимые модули и изменил формирование базового URL API в useApiFetch

--- app/assets/icons/Icon.vue
+++ app/assets/icons/Icon.vue
@@ -1,5 +1,6 @@
-<template>
-    <div>
-        SVG
-    </div>
-</template>
+<script setup>
+  let a = 23
+  console.log(a, 'asdfasd')
+</script>
+
+<template>SVG</template>
--- app/composable/api/useApiFetch.ts
+++ app/composable/api/useApiFetch.ts
@@ -1,23 +1,25 @@
-import type { UseFetchOptions } from "nuxt/app"
+import type { UseFetchOptions } from 'nuxt/app'
+import { useRuntimeConfig, useCookie, useFetch } from 'nuxt/app'
+import { computed } from 'vue'
 
 export const useApiFetch = <DataT = unknown>(request: string, options?: UseFetchOptions<DataT>) => {
-    const config = useRuntimeConfig()
-    const authToken = useCookie('authToken')
-    
-    if (!config.public.apiBaseUrl) {
-        throw new Error('API_BASE_URL is not set')
-    }
-          
-    const defaults: UseFetchOptions<DataT> = {
-        baseURL: config.public.apiBaseUrl,
-        key: request + JSON.stringify(options?.params),
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': authToken.value ? `Bearer ${authToken.value}` : ''
-        },
-    }
-    
-    const params = { ...defaults, ...options }
-      
-    return useFetch(request, params)
+  const config = useRuntimeConfig()
+  const authToken = useCookie('authToken')
+
+  if (!config.public.apiBaseUrl) {
+    throw new Error('API_BASE_URL is not set')
+  }
+
+  const defaults: UseFetchOptions<DataT> = {
+    baseURL: computed(() => config.public.apiBaseUrl as string),
+    key: request + JSON.stringify(options?.params),
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: authToken.value ? `Bearer ${authToken.value}` : '',
+    },
+  }
+
+  const params = { ...defaults, ...options }
+
+  return useFetch(request, params)
 }
