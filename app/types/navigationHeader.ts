a904659d3c2c139ff186c53d7f81c928656de631
sergey-kolesnik <00volchenok0013@mail.ru>
2026-03-03 10:02:11 +0300

    refactor(header): Fixed incorrect link path
    Corrected typo in "our-story" link path.

--- app/types/navigationHeader.ts
+++ app/types/navigationHeader.ts
@@ -1,7 +1,7 @@
 export const NAVIGATION_ITEMS_HEADER = [
     { id: "shop", label: "Shop", href: "/shop" },
     { id: "blog", label: "Blog", href: "/blog" },
-    { id: "our-story", label: "Our Story", href: "/ry" },
+    { id: "our-story", label: "Our Story", href: "/our-story" },
 ] as const;
 
 export type NavItemHeader = typeof NAVIGATION_ITEMS_HEADER[number]
