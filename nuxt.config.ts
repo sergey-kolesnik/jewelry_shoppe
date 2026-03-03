9b16dd78c28c4abfb30718a7897525984b1b2bb1
sergey-kolesnik <00volchenok0013@mail.ru>
2026-03-03 15:19:07 +0300

    chore(vite): Configure SCSS preprocessing rule.
    Configure Vite build tool to support global SCSS imports

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,13 @@ import { defineNuxtConfig } from 'nuxt/config'
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['~/assets/scss/main.scss'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/assets/scss/_var.scss" as *;'
+        }
+      }
+    }
+  }
 })
