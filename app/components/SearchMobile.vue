2e4217b7622a7f464f1f2c79fbceb34b4e4e67a3
sergey-kolesnik <00volchenok0013@mail.ru>
2026-03-10 16:31:40 +0300

    feat(search mobile): add SearchMobile component with layout and styles
    Add the SearchMobile Vue component for the mobile header. Includes template markup, scoped styles, and the search icon integration to render the mobile search UI.

---
+++ app/components/SearchMobile.vue
@@ -0,0 +1,40 @@
+<script lang="ts" setup>
+  import SearchIcon from '~/assets/icons/SearchIcon.vue'
+</script>
+
+<template>
+  <form class="search" action="">
+    <SearchIcon class="search__icon" />
+    <input class="search__input" type="text" placeholder="Search" />
+  </form>
+</template>
+
+<style lang="scss" scoped>
+  .search {
+    position: relative;
+    display: flex;
+    align-items: center;
+    width: 100%;
+
+    &__icon {
+      position: absolute;
+      left: 10px;
+      width: 12px;
+      height: 12px;
+      color: $dark-gray-color;
+    }
+
+    &__input {
+      width: 100%;
+      height: 32px;
+      padding-left: 30px;
+      background-color: $light-gray-color;
+      border: none;
+      border-radius: 4px;
+    }
+
+    &__input::placeholder {
+      @include text-style(12px, $dark-gray-color, 20px);
+    }
+  }
+</style>
