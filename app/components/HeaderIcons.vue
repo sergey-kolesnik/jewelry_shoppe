e077daf08ae2def6915dc7f729c296ffb7eead11
sergey-kolesnik <00volchenok0013@mail.ru>
2026-03-10 13:00:39 +0300

    style: refine header icons responsiveness and container sizing
    Summary based on app/components/HeaderIcons.vue, app/layouts/BaseHeader.vue, and app/assets/scss/main.scss:
    
    Forwarded attrs in header icons and adjusted mobile-only visibility/spacing.
    Tweaked header logo sizing and burger cursor styling.
    Updated container sizing rules in main styles.

--- app/components/HeaderIcons.vue
+++ app/components/HeaderIcons.vue
@@ -1,25 +1,47 @@
 <script lang="ts" setup>
   import type { Component } from 'vue'
+  import { useAttrs } from 'vue'
 
   import SearchIcon from '~/assets/icons/SearchIcon.vue'
   import CartIcon from '~/assets/icons/CartIcon.vue'
   import UserIcon from '~/assets/icons/UserIcon.vue'
 
+  defineOptions({ inheritAttrs: false })
+  const attrs = useAttrs()
+
   const icons: Component[] = [SearchIcon, CartIcon, UserIcon]
 </script>
 
 <template>
-  <component :is="icon" v-for="(icon, index) in icons" :key="index" class="header__icon" />
+  <div v-bind="attrs">
+    <component :is="icon" v-for="(icon, index) in icons" :key="index" class="header__icon" />
+  </div>
 </template>
 
 <style lang="scss" scoped>
-  .header {
-    &__icon {
-      min-width: 21px;
-    }
+.header {
+  &__icon {
+    min-width: 21px;
 
-    &__icon:not(:last-child) {
-      margin-right: 39px;
+    @media (max-width: $breakpoint-s) {
+      display: none;
     }
   }
+
+  &__icon:not(:last-child) {
+    margin-right: 39px;
+
+    @media (max-width: $breakpoint-s) {
+      margin-right: 0;
+    }
+  }
+
+  @media (max-width: $breakpoint-s) {
+    &__icon:nth-child(2) {
+      display: inline-block;
+      margin-right: 16px;
+      min-width: 18px;
+    }
+  }
+}
 </style>
--- app/layouts/BaseHeader.vue
+++ app/layouts/BaseHeader.vue
@@ -2,6 +2,7 @@
   import LogoIcon from '@/assets/icons/LogoIcon.vue'
   import HeaderLinks from '~/components/HeaderLinks.vue'
   import HeaderIcons from '~/components/HeaderIcons.vue'
+  import BurgerIcon from '~/assets/icons/BurgerIcon.vue'
 </script>
 
 <template>
@@ -12,6 +13,7 @@
         <div class="header__right">
           <HeaderLinks class="header__links" />
           <HeaderIcons class="header__icons" />
+          <BurgerIcon class="header__burger" />
         </div>
       </div>
     </div>
@@ -23,17 +25,38 @@
     padding-top: 64px;
 
     &__logo {
-      min-width: 158px;
+      width: 158px;
+
+      @media (max-width: $breakpoint-s) {
+        width: 99px;
+      }
     }
 
     &__right {
       display: flex;
       align-items: center;
       margin-left: auto;
+
+    }
+
+    &__links {
+      @media (max-width: $breakpoint-s) {
+        display: none;
+      }
     }
 
     &__icons {
       display: flex;
+    }
+
+    &__burger {
+      display: none;
+      cursor: pointer;
+      
+
+      @media (max-width: $breakpoint-s) {
+        display: block;
+      }
     }
 
     &__line {
@@ -43,6 +66,10 @@
       width: 100%;
       padding-bottom: 17px;
       border-bottom: 1px solid $gray-color;
+
+      @media (max-width: $breakpoint-s) {
+        border-bottom: none;
+      }
     }
   }
 </style>
