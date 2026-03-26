0845db4845c0373b4ba326d691f36623a2a292bb
serg <serg@localhost.localdomain>
2026-03-26 18:24:38 +0300

    wip(slider): working on a slider

--- app/app.vue
+++ app/app.vue
@@ -1,14 +1,12 @@
 <script lang="ts" setup>
-import { useFetch } from '#app';
+  import { computed } from 'vue'
   import BaseHeader from '@/layouts/BaseHeader.vue'
-  import { useGetAllImages } from './composable/api/imagesSlider/useGetImagesSlider';
+  import { useGetAllImages } from './composable/api/imagesSlider/useGetImagesSlider'
 
   // const data = await useGetAllImages({id: 10, q: 95, w: 1300, h: 700, fit: 'fill'})
-  const {data,error} = await useGetAllImages({limit: 10})
-  console.log(data.value)
-  console.log(error.value)
-  
-  // console.log(data)
+  const { data } = await useGetAllImages({ limit: 10 })
+
+  const photos = computed(() => data.value?.photos ?? [])
 </script>
 
 <template>
@@ -16,5 +14,9 @@ import { useFetch } from '#app';
     <NuxtLayout>
       <BaseHeader />
     </NuxtLayout>
+    <div v-for="photo in photos" :key="photo.id">
+      <img :src="photo.url" alt="" loading="lazy" />
+      {{ photo.url }}
+    </div>
   </div>
 </template>
--- app/composable/api/imagesSlider/useGetImagesSlider.ts
+++ app/composable/api/imagesSlider/useGetImagesSlider.ts
@@ -1,41 +1,12 @@
-// <!-- https://picsum.photos/v2/list?limit=10 -->
-
-import { useApiFetch } from "@/composable/api/useApiFetch";
-
-// export type PicsumImage = {
-//   id: string
-//   author: string
-//   width: number
-//   height: number
-//   url: string
-//   download_url: string
-// }
-
-// https://via.assets.so/album.png?id=1&q=95&w=360&h=360&fit=fill
-
-
-// export const useGetAllImages = (options: { id?: number, q?: number, w?: number, h?: number, fit?: string} = {}) => {
-//   const { id, q, w, h, fit } = options
-  
-//   return useApiFetch('/album.png', {
-//       params: {
-//           id,
-//           q,
-//           w,
-//           h,
-//           fit,
-//       },
-//   })
-// }
+import { useApiFetch } from '@/composable/api/useApiFetch'
+import type { ApiPhotosResponse } from '~/types/imageSlider.type'
 
 export const useGetAllImages = (options: { limit?: number } = {}) => {
   const { limit } = options
-  
-  return useApiFetch('/v2/list', {
-      params: {
-          limit,
-      },
+
+  return useApiFetch<ApiPhotosResponse>('/v1/sample-data/photos', {
+    params: {
+      limit,
+    },
   })
 }
-
-
