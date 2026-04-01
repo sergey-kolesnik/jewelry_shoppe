66f075066ad1828e2ab66c1909360beaf61e1444
sergey-kolesnik <00volchenok0013@mail.ru>
2026-04-01 13:31:17 +0300

    chore(app): Clear app.vue

--- app/app.vue
+++ app/app.vue
@@ -1,12 +1,5 @@
 <script lang="ts" setup>
-  import { computed } from 'vue'
   import BaseHeader from '@/layouts/BaseHeader.vue'
-  import { useGetAllImages } from './composable/api/imagesSlider/useGetImagesSlider'
-
-  // const data = await useGetAllImages({id: 10, q: 95, w: 1300, h: 700, fit: 'fill'})
-  const { data } = await useGetAllImages({ limit: 10 })
-
-  const photos = computed(() => data.value?.photos ?? [])
 </script>
 
 <template>
@@ -14,9 +7,5 @@
     <NuxtLayout>
       <BaseHeader />
     </NuxtLayout>
-    <div v-for="photo in photos" :key="photo.id">
-      <img :src="photo.url" alt="" loading="lazy" />
-      {{ photo.url }}
-    </div>
   </div>
 </template>
