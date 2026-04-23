6982f088e1053996c237f5a47f7dd624c7cf2b41
sergey-kolesnik <00volchenok0013@mail.ru>
2026-04-23 16:46:00 +0300

    wip(footer): create BaseFooter

---
+++ app/layouts/BaseFooter.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+  import { NAVIGATION_SECONDARY } from '~/constants/navigation'
+</script>
+
+<template>
+  <footer class="footer">
+    <div class="container footer__container">
+      <div class="footer__line">
+        <div class="footer__left">
+          <ul class="footer__links">
+            <li v-for="item in NAVIGATION_SECONDARY" :key="item.id" class="footer__links-item">
+              <a class="footer__link" :href="item.href">
+                {{ item.label }}
+              </a>
+            </li>
+          </ul>
+          <p class="footer__legal">
+            &copy;&nbsp;<b>2021&nbsp;Shelly.</b> Terms of&nbsp;use <b>and</b> privacy policy.
+          </p>
+        </div>
+        <div class="footer__right"></div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+  .footer {
+    &__line {
+      border-top: 1px solid $gray-color;
+    }
+  }
+</style>
