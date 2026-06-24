a66c9b5e3d1791b8ede2ee0a430cbaee9b12528e
sergey-kolesnik <00volchenok0013@mail.ru>
2026-06-24 17:00:35 +0300

    feat(ProductCard): create add to card in hover

--- app/components/ProductCard.vue
+++ app/components/ProductCard.vue
@@ -8,7 +8,16 @@
 
 <template>
   <div class="product-card">
-    <img :src="props.product.image" :alt="props.product.description" class="product-card__image" />
+    <div class="product-card__image-wrapper">
+      <img
+        :src="props.product.image"
+        :alt="props.product.description"
+        class="product-card__image"
+      />
+      <div class="product-card__add-to-card">
+        <span class="product-card__add-to-card-text"> ADD TO CART </span>
+      </div>
+    </div>
     <h3 class="product-card__title">
       {{ props.product.title }}
     </h3>
@@ -27,11 +36,38 @@
   .product-card {
     width: calc((100% - (2 * 24px)) / 3);
 
+    &__image-wrapper {
+      position: relative;
+      margin-bottom: 24px;
+    }
+
     &__image {
       width: 100%;
       aspect-ratio: 1 / 1;
-      margin-bottom: 24px;
       border-radius: 8px;
+    }
+
+    &__add-to-card {
+      position: absolute;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      z-index: 1;
+      display: none;
+      padding-top: 21px;
+      padding-bottom: 21px;
+      text-align: center;
+      background-color: rgba($white-color, 0.5);
+      border-bottom-right-radius: 8px;
+      border-bottom-left-radius: 8px;
+    }
+
+    &:hover &__add-to-card {
+      display: block;
+    }
+
+    &__add-to-card-text {
+      @include text-style(16px, $black-color, 26px, map.get($font-weights, bold));
     }
 
     &__title {
